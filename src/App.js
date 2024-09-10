336677076ea7cbccb1b26fa5731eddb4588f9c74
Auchera <c.emirdalkilic@gmail.com>
2024-09-10 13:50:14 +0300

    push test

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
 import "./App.scss";
-import Header from "./components/header/Header";
-import Products from "./components/products/Products";
 
-import {categories, products} from "./helper/data"
 
 
 function App() {
   return (
     <div>
     
-
-    <Header  categorieS={categories}/>
-
-    <Products products={products}/>
-
 
     </div>
   );
